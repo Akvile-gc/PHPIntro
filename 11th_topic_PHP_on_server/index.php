02147d98ead18623537b6d148ced03d2d96e9ca9
Akvile-gc <akvile.gvildyte@gmail.com>
2023-01-04 21:57:25 +0200

    Lesson 11. 4 - in progress

--- 11th_topic_PHP_on_server/index.php
+++ 11th_topic_PHP_on_server/index.php
@@ -1,17 +1,32 @@
-g<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="./styles.css">
         <title>Document</title>
     </head>
     <body>
-        <form method="POST" action="submit.php">
-            <input type="text" name="first_name" placeholder="Name">
-            <input type="text" name="surname" placeholder="Surname">
+        <?php if(isset($_POST['submit'])) {
+                echo $_POST['todo_list'] . PHP_EOL;
+            }
+        ?>
+        <form method="POST" action="" class="frame">
+            <legend>New TODO</legend>
+            <br>
+            <input type="text" name="todo_list">
             <input type="submit">
         </form>
+        <div class="frame">
+            <legend>TODOs</legend>
+            <p>Walk the dog</p>
+            <p>Clean the house</p>
+            <p>Wash the car</p>
+
+            <?php echo $_POST['todo_list'] . PHP_EOL ?>
+
+        </div>
     </body>
 </html>
--- 11th_topic_PHP_on_server/submit.php
+++ 11th_topic_PHP_on_server/submit.php
@@ -1,3 +1,6 @@
 <?php
+//3rd task
+//echo 'Hello, ' . $_POST['first_name'] . ' ' . $_POST['surname'] . ', how are you today?';
 
-echo 'Hello, ' . $_POST['first_name'] . ' ' . $_POST['surname'] . ', how are you today?';
+//4th task
+echo 'Form submitted!';
