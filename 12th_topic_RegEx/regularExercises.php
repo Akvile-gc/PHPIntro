e7072fe119cbfe25e1e21a68fdd5e0165f1ea6b4
Akvile-gc <akvile.gvildyte@gmail.com>
2023-01-10 21:39:20 +0200

    L12. R3 - done

--- 12th_topic_RegEx/regularExercises.php
+++ 12th_topic_RegEx/regularExercises.php
@@ -33,9 +33,9 @@ function exercise2(?string $number):bool
 
     }
 
-foreach ($numbers as $num){
-    var_dump(exercise2($num));
-}
+//foreach ($numbers as $num){
+//    var_dump(exercise2($num));
+//}
 
 /*
 3. Patobulinkite funkciją (2). Funkcija turėtų galėti patikrinti ir tokius telefono numerius:
@@ -45,6 +45,29 @@ foreach ($numbers as $num){
 "00370 623 45678"
 Jeigu telefono numeris validus, iš funkcijos turėtų grįžti tvarkingai suformatuotas telefono numeris:
 "+370-623 45678" --> "+37062345678"
+*/
+
+$phones = ["+370 623 45678", "+370-623-45678", "+370-623 45678", "00370 623 45678"];
+function exercise3(string $number):string
+{
+    $pattern = '/^(\+|0{2})(370)( |\-)([6]\d{2})( |\-)(\d{5})$/';
+    $replace = '+$2$4$6';
+
+//    '/^\+((370|8)6)\d{7}$/';
+    $match = preg_match($pattern, $number);
+
+    if ($match){
+        $result = preg_replace($pattern, $replace, $number);
+        return $result;
+    }
+    else
+        return 'Wrong number';
+}
+
+foreach ($phones as $num){
+    var_dump(exercise3($num));
+}
+/*
 4. Parašykite funkciją, kuri užmaskuotų dalį vartotojo duomenų. Pavardės ir gimimo metų simboliai
 turėtų būti pakeisti i simbolius 'X'.
 "John Smith, 1979 05 15" --> "John XXXXX, XXXX 05 15"
